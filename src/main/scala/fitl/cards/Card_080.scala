35fab73b369c2d4b4e5d01a8a4d8e2423cfc43d7
Curt Sellmer <sellmerfud@gmail.com>
2021-11-26 17:51:57 -0600

    Implement event #80

--- src/main/scala/fitl/cards/Card_080.scala
+++ src/main/scala/fitl/cards/Card_080.scala
@@ -41,6 +41,17 @@ import fitl.Bot
 import fitl.Bot.{ US_Bot, ARVN_Bot, NVA_Bot, VC_Bot }
 import fitl.Human
 
+// Single Event Text
+// Wind down seen: Remove 1-4 US pieces from map to Available. For each piece,
+// Patronage +2, shift a space 1 level toward Active Opposition, and place 4
+// NVA Troops outside the South. Stay Eligible.
+//
+// Tips
+// The Faction executing the Event decides the details. "Pieces" can include Bases.
+// A Province with 0 Population cannot shift (1.6). "Outside the South" means any
+// North Vietnam, Laos, or Cambodia Provinces. 
+// The Faction executing the Event stays Eligible for the next card.
+
 object Card_080 extends EventCard(80, "Light at the End of the Tunnel",
   SingleEvent,
   List(ARVN, NVA, VC, US),
@@ -50,8 +61,67 @@ object Card_080 extends EventCard(80, "Light at the End of the Tunnel",
           VC   -> (Critical    -> Unshaded))) {
 
 
-  def unshadedEffective(faction: Faction): Boolean = false
-  def executeUnshaded(faction: Faction): Unit = singleNotYet()
+  val shiftCandidate = (sp: Space) =>
+    sp.population > 0 &&
+    sp.support > ActiveOpposition
+
+  def unshadedEffective(faction: Faction): Boolean = {
+    val usOnMap = game.totalOnMap(_.pieces.totalOf(USPieces)) > 0
+    faction match {
+      case US   => false
+      case ARVN => usOnMap && game.patronage < EdgeTrackMax
+      case NVA  => usOnMap && game.availablePieces.has(NVATroops)
+      case VC   => usOnMap && (game.nonLocSpaces exists shiftCandidate)
+    }
+  }
+
+  def shiftSpaces(faction: Faction, num: Int, shiftCandidates: List[Space]): Unit = {
+    val selectedSpaces = if (shiftCandidates.isEmpty)
+      Nil
+    else if (game.isHuman(faction)) {
+      val numSpaces = num min shiftCandidates.size
+      val prompt = s"\nSelect ${amountOf(numSpaces, "space")} to shift toward Active Opposition:"
+      askSimpleMenu(spaceNames(shiftCandidates), prompt, numSpaces)
+    }
+    else {
+      def nextSpace(remaining: Int, candidates: List[Space]): List[String] = {
+        if (remaining > 0 && candidates.nonEmpty) {
+          val name = VC_Bot.pickSpaceTowardActiveOpposition(candidates).name
+          name::nextSpace(remaining - 1, candidates filterNot (_.name == name))
+        }
+        else
+          Nil
+      }
+      nextSpace(num, shiftCandidates).reverse
+    }
+
+    if (selectedSpaces.isEmpty)
+      log("\nThere are no spaces that can be shifted toward Active Opposition")
+    else
+      for (name <- selectedSpaces)
+        decreaseSupport(name, 1)
+  }
+
+  def executeUnshaded(faction: Faction): Unit = {
+    val numUSOnMap = game.totalOnMap(_.pieces.totalOf(USPieces))
+    if (numUSOnMap == 0)
+      log("There are no US pieces on the map")
+    else {
+      val shiftCandidates = game.nonLocSpaces filter shiftCandidate
+      val num =  if (game.isHuman(faction))
+        askInt("\nRemove how many US pieces from the map", 1, numUSOnMap min 4)
+      else
+        numUSOnMap min 4
+
+      loggingControlChanges {
+        removePiecesFromMap(faction, num, USPieces, false, spaceNames(game.spaces), usToAvailable = true)
+        log()
+        increasePatronage(num * 2)
+        shiftSpaces(faction, num, shiftCandidates)
+        placePiecesOnMap(faction, num * 4, Set(NVATroops), NorthVietnam::LaosCambodia)
+      }
+    }
+  }
 
   // Single event - These functions are not used
   def shadedEffective(faction: Faction): Boolean = false

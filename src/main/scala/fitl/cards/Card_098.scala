a62494105ded329f9140c7479e00ca1eb2e95cbe
Curt Sellmer <sellmerfud@gmail.com>
2021-12-06 15:18:32 -0600

    Implement event #98

--- src/main/scala/fitl/cards/Card_098.scala
+++ src/main/scala/fitl/cards/Card_098.scala
@@ -41,6 +41,18 @@ import fitl.Bot
 import fitl.Bot.{ US_Bot, ARVN_Bot, NVA_Bot, VC_Bot }
 import fitl.Human
 
+// Unshaded Text
+// Royal Australians: Place 2 out of play US Troops into a Province
+// or remove all Guerrillas from all Jungle with US Troops.
+//
+// Shaded Text
+// VC strike newly arrived troops: 1 US Base and 1 US Troop in
+// a Jungle with 2+ VC Guerrillas to Casualties.
+//
+// Tips
+// For the unshaded effect, the Troops could go into any 1 Province
+// except North Vietnam, not necessarily into Jungle.
+
 object Card_098 extends EventCard(98, "Long Tan",
   DualEvent,
   List(VC, US, ARVN, NVA),
@@ -50,9 +62,79 @@ object Card_098 extends EventCard(98, "Long Tan",
           VC   -> (NotExecuted -> Shaded))) {
 
 
-  def unshadedEffective(faction: Faction): Boolean = false
-  def executeUnshaded(faction: Faction): Unit = unshadedNotYet()
+  val unshadedCandidate = (sp: Space) =>
+    sp.isJungle &&
+    sp.pieces.has(USTroops) &&
+    sp.pieces.has(Guerrillas)
 
-  def shadedEffective(faction: Faction): Boolean = false
-  def executeShaded(faction: Faction): Unit = shadedNotYet()
+  def unshadedEffective(faction: Faction): Boolean = if (faction == US)
+    game.outOfPlay.has(USTroops) || (game.nonLocSpaces exists unshadedCandidate)
+  else
+    (game.nonLocSpaces exists unshadedCandidate)
+  
+
+  def executeUnshaded(faction: Faction): Unit = {
+    val guerrillaCandidates = game.nonLocSpaces filter unshadedCandidate
+    val oopTroops = game.outOfPlay.totalOf(USTroops)
+    val numRemoved = guerrillaCandidates.map(_.pieces.totalOf(Guerrillas)).sum
+    val choices = List(
+      choice(oopTroops > 0,  "troops",     "Place Out of Play US Troops in a Province"),
+      choice(numRemoved > 0, "guerrillas", "Remove all Guerrillas from a Jungle spaces with US Troops")
+    ).flatten
+
+    if (choices.isEmpty)
+      log("The event cannot be carried out.")
+    else {
+      val action = if (game.isHuman(faction))
+        askMenu(choices, "\nChoose one:").head
+      else if (faction == ARVN)
+        "guerrillas"
+      else 
+        if (oopTroops == 0 || numRemoved >= 4) "guerrillas" else "troops"
+
+      loggingControlChanges {
+        action match {
+          case "troops" =>
+            val provinces = game.nonLocSpaces filter (_.isProvince)
+            val name = if (game.isHuman(faction))
+              askSimpleMenu(spaceNames(provinces), "\nSelect a Province for the Out of Play troops:").head
+            else
+              US_Bot.pickSpacePlaceCubesSpecialForces(troops = true)(provinces).name
+            val troops = Pieces(usTroops = oopTroops min 2)
+            placePiecesFromOutOfPlay(name, troops)
+
+          case _ =>
+            for (sp <- guerrillaCandidates)
+              removePieces(sp.name, sp.pieces.only(Guerrillas))
+        }      
+      }
+    }
+  }
+
+  val shadedCandidate = (sp: Space) =>
+    sp.isJungle &&
+    sp.pieces.totalOf(VCGuerrillas) >= 2 &&
+    sp.pieces.has(USBase::USTroops::Nil)
+
+  def shadedEffective(faction: Faction): Boolean = game.nonLocSpaces exists shadedCandidate
+
+  def executeShaded(faction: Faction): Unit = {
+    val candidates = game.nonLocSpaces filter shadedCandidate
+
+    if (candidates.isEmpty)
+      log("There are no spaces the qualify for the event")
+    else {
+      val name = if (game.isHuman(faction))
+        askSimpleMenu(spaceNames(candidates), "\nChoose Jungle space to remove US Base and US Troop:").head
+      else
+        NVA_Bot.pickSpaceRemoveReplace(candidates).name
+
+      println()
+      val sp = game.getSpace(name)
+      val pieces = Pieces(
+        usBases  = sp.pieces.totalOf(USBase) min 1,
+        usTroops = sp.pieces.totalOf(USTroops) min 1)
+      removeToCasualties(name, pieces)
+    }
+  }
 }

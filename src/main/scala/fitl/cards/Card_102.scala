834cbbed4e441e4c5ea6827e6d8888feab1d3b2a
Curt Sellmer <sellmerfud@gmail.com>
2021-12-09 00:21:32 -0600

    Implement event #102

--- src/main/scala/fitl/cards/Card_102.scala
+++ src/main/scala/fitl/cards/Card_102.scala
@@ -41,6 +41,17 @@ import fitl.Bot
 import fitl.Bot.{ US_Bot, ARVN_Bot, NVA_Bot, VC_Bot }
 import fitl.Human
 
+// Unshaded Text
+// Clear and secure: Remove all Guerrillas from 1 space with
+// a Tunnel and COIN Control.
+//
+// Shaded Text
+// Iron Triangle: Place Tunnel markers on each Insurgent Base in
+// 1 Province. Place 1 NVA and 1 VC Guerrilla there.
+//
+// Tips
+// "A Tunnel" means a Tunneled Base.
+
 object Card_102 extends EventCard(102, "Cu Chi",
   DualEvent,
   List(VC, NVA, US, ARVN),
@@ -49,10 +60,82 @@ object Card_102 extends EventCard(102, "Cu Chi",
           NVA  -> (Performed   -> Shaded),
           VC   -> (NotExecuted -> Shaded))) {
 
+  val unshadedCandidate = (sp: Space) =>
+    sp.coinControlled &&
+    sp.pieces.has(InsurgentTunnels) &&
+    sp.pieces.has(Guerrillas)
 
-  def unshadedEffective(faction: Faction): Boolean = false
-  def executeUnshaded(faction: Faction): Unit = unshadedNotYet()
+  def unshadedEffective(faction: Faction): Boolean = game.nonLocSpaces exists unshadedCandidate
 
-  def shadedEffective(faction: Faction): Boolean = false
-  def executeShaded(faction: Faction): Unit = shadedNotYet()
+  def executeUnshaded(faction: Faction): Unit = {
+    val candidates = game.nonLocSpaces filter unshadedCandidate
+
+    if (candidates.isEmpty)
+      log("There are no spaces that qualify for the event")
+    else {
+      val name = if (game.isHuman(faction))
+        askSimpleMenu(spaceNames(candidates), "\nSelect space to carry out the event:").head
+      else
+        Bot.pickSpaceRemoveReplace(faction)(candidates).name
+
+      val sp = game.getSpace(name)
+      removePieces(name, sp.pieces.only(Guerrillas))
+    }
+  }
+
+  val shadedCandidate = (sp: Space) =>
+    sp.isProvince &&
+    sp.pieces.has(InsurgentNonTunnels)
+
+  def shadedEffective(faction: Faction): Boolean =
+    (game.nonLocSpaces exists shadedCandidate) &&
+    (game.tunnelMarkersAvailable > 0 || game.availablePieces.has(Guerrillas))
+
+  def executeShaded(faction: Faction): Unit = {
+    val candidates = game.nonLocSpaces filter shadedCandidate
+
+    if (candidates.isEmpty)
+      log("There are no spaces that qualify for the event")
+    else {
+      val name = if (game.isHuman(faction))
+        askSimpleMenu(spaceNames(candidates), "\nSelect space to carry out the event:").head
+      else
+        NVA_Bot.pickSpacePlaceBases(candidates).name
+
+      println()
+      val sp    = game.getSpace(name)
+      val bases = sp.pieces.only(InsurgentNonTunnels)
+      if (game.tunnelMarkersAvailable == 0)
+        log("There are no available Tunnel markers")
+      else {
+        val toTunnel = if (game.tunnelMarkersAvailable >= bases.total)
+          bases
+        else if (bases.has(NVABase) && bases.has(VCBases)) {
+          if (game.isHuman(faction)) {
+            val choices = List(
+              Pieces(vcBases  = 1) -> "VC Base",
+              Pieces(nvaBases = 1) -> "NVA Base"
+            )
+            println("\nThere is only one Tunnel marker available")
+            askMenu(choices, "Place Tunnel marker on which base:").head
+          }
+          else
+            Pieces(nvaBases = 1)  // NVA Bot picks its own base
+        }
+        else {
+          // Two of same type of base
+          val baseType = bases.explode().head
+          Pieces().set(1, baseType)
+        }
+
+        loggingControlChanges {
+          addTunnelMarker(name, toTunnel)
+          val guerrillas = Pieces(
+           nvaGuerrillas_U =  game.availablePieces.totalOf(NVAGuerrillas_U) min 1,
+           vcGuerrillas_U  =  game.availablePieces.totalOf(VCGuerrillas_U)  min 1)
+          placePieces(name, guerrillas)
+        }
+      }
+    }
+  }
 }

5e365cd9d20ede215bf341002131b7aa577e17eb
Curt Sellmer <sellmerfud@gmail.com>
2021-12-11 11:59:37 -0600

    Implement event #106

--- src/main/scala/fitl/cards/Card_106.scala
+++ src/main/scala/fitl/cards/Card_106.scala
@@ -41,6 +41,14 @@ import fitl.Bot
 import fitl.Bot.{ US_Bot, ARVN_Bot, NVA_Bot, VC_Bot }
 import fitl.Human
 
+// Single Event Text
+// Revolutionary land reform seeks traction in prosperous districts:
+// In each of 2 Provinces adjacent to Saigon, shift Support/Opposition
+// 1 level either direction and place a VC Guerrilla or Police.
+//
+// Tips
+// Shifting toward Opposition and placing Police would be a legal (if unusual) move.
+
 object Card_106 extends EventCard(106, "Binh Duong",
   SingleEvent,
   List(VC, NVA, ARVN, US),
@@ -49,9 +57,95 @@ object Card_106 extends EventCard(106, "Binh Duong",
           NVA  -> (NotExecuted -> Unshaded),
           VC   -> (Critical    -> Unshaded))) {
 
+  def candidates     = spaces(getAdjacent(Saigon)) filter (_.isProvince)
+  def coinCandidates = candidates filter (_.support < ActiveSupport)
+  def vcCandidates   = candidates filter (_.support > ActiveOpposition)
 
-  def unshadedEffective(faction: Faction): Boolean = false
-  def executeUnshaded(faction: Faction): Unit = singleNotYet()
+  def unshadedEffective(faction: Faction): Boolean = faction match {
+    case US|ARVN => coinCandidates.nonEmpty
+    case _       => vcCandidates.nonEmpty
+  }
+
+
+  // Return true if shift toward Support or false if shift toward Opposition
+  def askShift(name: String, faction: Faction): Boolean = {
+    val sp = game.getSpace(name)
+    val choices = List(
+      choice(sp.support < ActiveSupport,    true,  "Shift 1 level toward Active Support"),
+      choice(sp.support > ActiveOpposition, false, "Shift 1 level toward Active Opposition")
+    ).flatten
+    val sorted = if (isCoin(faction)) choices else choices.reverse
+    askMenu(sorted, s"\nChoose shift for $name").head
+  }
+
+  // Ask if the user wishes to place a VC Guerrilla or Police in the space.
+  // If the piece is not available then:
+  // VC Guerrilla - VC has the option of voluntary removal
+  // Police       - Coin factions have the option of voluntary removal
+  // Otherwise    - If the other piece type is available it is selected
+  def askPiece(name: String, faction: Faction): Pieces = {
+    val haveGuerrilla = game.availablePieces.has(VCGuerrillas_U)
+    val havePolice    = game.availablePieces.has(ARVNPolice)
+    val canRemoveG    = !haveGuerrilla && faction == VC
+    val canRemoveP    = !havePolice && isCoin(faction)
+
+    val choices = List(
+      choice(havePolice,    ARVNPolice,     "ARVN Police"),
+      choice(canRemoveP,    ARVNPolice,     "ARVN Police (requires volunatary removal)"),
+      choice(haveGuerrilla, VCGuerrillas_U, "VC Guerrilla"),
+      choice(canRemoveG,    VCGuerrillas_U, "VC Guerrilla (requires volunatary removal)")
+    ).flatten
+    val sorted = if (isCoin(faction)) choices else choices.reverse
+
+    if (sorted.isEmpty)
+      Pieces()
+    else {
+      val pieceType = askMenu(sorted, s"\nChoose type of piece to place in $name").head
+      if (!game.availablePieces.has(pieceType))
+        voluntaryRemoval(1, pieceType, Set(name))
+      Pieces().set(1, pieceType)
+    }
+  }
+
+  def botPiece(faction: Faction): Pieces = {
+    val haveGuerrilla = game.availablePieces.has(VCGuerrillas_U)
+    val havePolice    = game.availablePieces.has(ARVNPolice)
+    faction match {
+      case VC if haveGuerrilla   => Pieces(vcGuerrillas_U = 1)
+      case US|ARVN if havePolice => Pieces(arvnPolice = 1)
+      case _ if haveGuerrilla    => Pieces(vcGuerrillas_U = 1)
+      case _ if havePolice       => Pieces(arvnPolice = 1)
+      case _                     => Pieces()
+    }
+  }
+
+  def executeUnshaded(faction: Faction): Unit = {
+    val selectedSpaces = if (game.isHuman(faction))
+      askSimpleMenu(spaceNames(candidates), "\nSelect 2 Provinces adjacent to Saigon:", numChoices = 2)
+    else
+      faction match {
+        case US   => Bot.pickSpaces(2, coinCandidates)(US_Bot.pickSpaceTowardActiveSupport) map (_.name)
+        case ARVN => Bot.pickSpaces(2, coinCandidates)(ARVN_Bot.pickSpaceTowardPassiveSupport) map (_.name)
+        case _    => Bot.pickSpaces(2, coinCandidates)(VC_Bot.pickSpaceTowardActiveOpposition) map (_.name)
+      }
+
+    loggingControlChanges {
+      for (name <- selectedSpaces) {
+        val sp = game.getSpace(name)
+        val (towardSupport, piece) = if (game.isHuman(faction))
+          (askShift(name, faction), askPiece(name, faction))
+        else
+          (isCoin(faction), botPiece(faction))
+        
+        println()
+        if (towardSupport)
+          increaseSupport(name, 1)
+        else
+          decreaseSupport(name, 1)
+        placePieces(name, piece)
+      }
+    }
+  }
 
   // Single event - These functions are not used
   def shadedEffective(faction: Faction): Boolean = false

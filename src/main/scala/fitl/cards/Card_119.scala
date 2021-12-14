167e8725a05453ad14804a48816dd4eccc00d530
Curt Sellmer <sellmerfud@gmail.com>
2021-12-14 11:38:14 -0600

    Implement event #119

--- src/main/scala/fitl/cards/Card_119.scala
+++ src/main/scala/fitl/cards/Card_119.scala
@@ -41,6 +41,16 @@ import fitl.Bot
 import fitl.Bot.{ US_Bot, ARVN_Bot, NVA_Bot, VC_Bot }
 import fitl.Human
 
+// Unshaded Text
+// US LT convicted: 2 Available US Troops out of play. Patronage +2.
+//
+// Shaded Text
+// Massacre: Set a Province with US Troops to Active Opposition.
+// VC place a Base and Guerrilla there. Aid –6.
+//
+// Tips
+// Provinces with 0 Population do not shift from Neutral.
+
 object Card_119 extends EventCard(119, "My Lai",
   DualEvent,
   List(VC, ARVN, NVA, US),
@@ -50,9 +60,75 @@ object Card_119 extends EventCard(119, "My Lai",
           VC   -> (Critical    -> Shaded))) {
 
 
-  def unshadedEffective(faction: Faction): Boolean = false
-  def executeUnshaded(faction: Faction): Unit = unshadedNotYet()
+  def unshadedEffective(faction: Faction): Boolean =
+    game.availablePieces.has(USTroops) ||
+    game.patronage < EdgeTrackMax
 
-  def shadedEffective(faction: Faction): Boolean = false
-  def executeShaded(faction: Faction): Unit = shadedNotYet()
+  def executeUnshaded(faction: Faction): Unit = {
+    val numTroops = game.availablePieces.totalOf(USTroops) min 2
+
+    loggingPointsChanges {
+      if (numTroops == 0)
+        log("There are no available US Troops")
+      else
+        moveAvailableToOutOfPlay(Pieces(usTroops = numTroops))
+      increasePatronage(2)
+    }
+  }
+
+  val provinceWithUSTroops = (sp: Space) =>
+    sp.isProvince &&
+    sp.pieces.has(USTroops)
+
+  val notActiveOppositionCandidate = (sp: Space) =>
+      provinceWithUSTroops(sp) &&
+      sp.canHaveSupport &&
+      sp.support != ActiveOpposition
+
+  def shadedEffective(faction: Faction): Boolean =
+    (game.nonLocSpaces exists notActiveOppositionCandidate) ||
+    ((game.nonLocSpaces exists provinceWithUSTroops) && game.availablePieces.has(VCPieces)) ||
+    (game.trackResources(ARVN) && game.usAid > 0)
+
+
+  def executeShaded(faction: Faction): Unit = {
+    val candidates          = game.nonLocSpaces filter provinceWithUSTroops
+    val activeOppCandidates = game.nonLocSpaces filter notActiveOppositionCandidate
+
+    val name = if (candidates.isEmpty)
+      ""
+    else if (game.isHuman(faction))
+      askSimpleMenu(spaceNames(candidates), "\nChoose a Province with US Troops:").head
+    else if (activeOppCandidates.nonEmpty)
+      VC_Bot.pickSpaceTowardActiveOpposition(activeOppCandidates).name
+    else if (game.availablePieces.has(VCBase))
+      VC_Bot.pickSpacePlaceBases(candidates).name
+    else
+      VC_Bot.pickSpacePlaceGuerrillas(candidates).name
+
+      
+    def piecesToPlace(canPlaceBase: Boolean): Pieces = {
+      val canVolRemove = faction == VC && game.isHuman(VC)
+      val placeBase = canPlaceBase &&
+                      game.availablePieces.has(VCBase) || (canVolRemove && Human.numToPlace(VCBase, 1) == 1)
+      val placeG    = game.availablePieces.has(VCGuerrillas_U) || (canVolRemove && Human.numToPlace(VCGuerrillas_U, 1) == 1)
+
+      Pieces(
+        vcBases        = if (placeBase) 1 else 0,
+        vcGuerrillas_U = if (placeG)    1 else 0
+      )
+    }
+
+    loggingControlChanges {
+      if (name == "")
+        log("There are no Provinces with US Troops")
+      else {
+        val sp = game.getSpace(name)
+        println()
+        setSupport(name, ActiveOpposition)
+        placePieces(name, piecesToPlace(sp.canTakeBase))
+      }
+      increaseUsAid(6)
+    }
+  }
 }

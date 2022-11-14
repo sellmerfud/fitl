afa10245f8efbd4b3abe089dd6575ce0d08ba775
Curt Sellmer <sellmerfud@gmail.com>
2022-11-14 15:08:15 -0600

    Update version number to 0.4

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ import java.util.HashSet
 
 lazy val commonSettings = Seq(
   organization := "org.sellmerfud",
-  version      := "0.3",
+  version      := "0.4",
   scalaVersion := "2.13.10"
 )
 
--- src/other/fitl.cmd
+++ src/other/fitl.cmd
@@ -9,7 +9,7 @@ rem Set the current working directory to the directory where this script is runn
 rem This is important so that all of our relative path references work correctly.
 pushd %~dp0
 
-set CLASSPATH=lib\fire-in-the-lake_2.13-0.3.jar;^
+set CLASSPATH=lib\fire-in-the-lake_2.13-0.4.jar;^
 lib\scala-library-2.13.10.jar;^
 lib\scala-parser-combinators_2.13-2.1.1.jar;
 

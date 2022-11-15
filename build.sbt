07355cc922b3bad7b0a99d65ea865fe222560482
Curt Sellmer <sellmerfud@gmail.com>
2022-11-15 14:52:10 -0600

    Update version number to 0.5

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ import java.util.HashSet
 
 lazy val commonSettings = Seq(
   organization := "org.sellmerfud",
-  version      := "0.4",
+  version      := "0.5",
   scalaVersion := "2.13.10"
 )
 
--- src/main/scala/fitl/FireInTheLake.scala
+++ src/main/scala/fitl/FireInTheLake.scala
@@ -51,7 +51,7 @@ import Bot.{ TrungCard, TrungDeck }
 
 object FireInTheLake {
 
-  val SOFTWARE_VERSION = "0.4"
+  val SOFTWARE_VERSION = "0.5"
   val INTEGER = """(\d+)""".r
 
   def d6 = nextInt(6) + 1

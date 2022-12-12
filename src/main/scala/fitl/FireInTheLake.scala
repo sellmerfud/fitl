b7167d4d26c31e9ba6315c870ba275bcb40af0e5
Curt Sellmer <sellmerfud@gmail.com>
2022-12-11 23:35:59 -0600

    Update version number to 0.13

--- src/main/scala/fitl/FireInTheLake.scala
+++ src/main/scala/fitl/FireInTheLake.scala
@@ -51,7 +51,7 @@ import Bot.{ TrungCard, TrungDeck }
 
 object FireInTheLake {
 
-  val SOFTWARE_VERSION = "0.12"
+  val SOFTWARE_VERSION = "0.13"
   val INTEGER = """(\d+)""".r
 
   def d6 = nextInt(6) + 1
--- src/other/fitl.cmd
+++ src/other/fitl.cmd
@@ -9,7 +9,7 @@ rem Set the current working directory to the directory where this script is runn
 rem This is important so that all of our relative path references work correctly.
 pushd %~dp0
 
-set CLASSPATH=lib\fire-in-the-lake_2.13-0.12.jar;^
+set CLASSPATH=lib\fire-in-the-lake_2.13-0.13.jar;^
 lib\scala-library-2.13.10.jar;^
 lib\scala-parser-combinators_2.13-2.1.1.jar;
 

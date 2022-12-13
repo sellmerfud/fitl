bfab38cfbea452b49d669a86593022bdb8478d55
Curt Sellmer <sellmerfud@gmail.com>
2022-12-13 00:06:43 -0600

    Update version number to 0.15

--- src/main/scala/fitl/FireInTheLake.scala
+++ src/main/scala/fitl/FireInTheLake.scala
@@ -51,7 +51,7 @@ import Bot.{ TrungCard, TrungDeck }
 
 object FireInTheLake {
 
-  val SOFTWARE_VERSION = "0.14"
+  val SOFTWARE_VERSION = "0.15"
   val INTEGER = """(\d+)""".r
 
   def d6 = nextInt(6) + 1
